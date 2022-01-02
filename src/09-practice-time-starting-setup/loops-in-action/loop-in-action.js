bb62bf2183daf5cea55ca1971c288ae960ea4afd
Khalid_Syahmi <mksyahmiof@gmail.com>
2022-01-02 15:23:47 +0800

    final-loop-die-game

--- src/09-practice-time-starting-setup/loops-in-action/loop-in-action.js
+++ src/09-practice-time-starting-setup/loops-in-action/loop-in-action.js
@@ -37,7 +37,7 @@ highlightLinksBtn.addEventListener("click", linkHighlighter);
 
 const dummyData = {
   firstName: "Klowe",
-  lastName: "Sobin",
+  lastName: "So-Bin",
   age: 26,
 };
 
@@ -46,13 +46,61 @@ const displayUserData = document.querySelector("#user-data button");
 function showUserInfo() {
   const outputDataElement = document.getElementById("output-user-data");
 
+  outputDataElement.innerHTML = ""; // will dump the memory before creating
+
   for (const key in dummyData) {
     const createNewLi = document.createElement("li");
     const outputText = key.toUpperCase() + ": " + dummyData[key]; // bind propertyName and the propertyName value
-    createNewLi.textContent = outputText; // extend to list textcontent
+    createNewLi.textContent = outputText; // extend to list textcontent //transform data to String
 
-    outputDataElement.append(createNewLi);
+    outputDataElement.append(createNewLi); // attach the String next to the lists
   }
 }
 
 displayUserData.addEventListener("click", showUserInfo);
+
+//Statistics //A classic dice roll // how many rolls until the die lands the entered number
+
+const diceBtn = document.querySelector("#statistics button");
+
+//f1
+function rollDice() {
+  return Math.floor(Math.random() * 6) + 1; // +1 begin 1 to 6 instead of zero
+}
+
+//f2
+function deriveNumberOfRolls() {
+  const targetNumberInput = document.getElementById("user-target-number"); //input DOM
+  const rollsList = document.getElementById("dice-rolls"); // output DOM
+
+  const enteredNumber = targetNumberInput.value; // i forgor
+
+  rollsList.innerHTML = "";
+
+  let hasRolled = false; // not rolled yet
+  let numberOfRolls = 0; // use let since the value is transformed
+
+  //loop
+  while (!hasRolled) {
+    const rolledNumber = rollDice(); // assign into new variable everytime rollDice() calculation is performed //random
+    /*   if (rolled==enteredNumber){
+      hasRolled =true;
+    } */
+    numberOfRolls++; //increment 1 each loop
+    const newRollList = document.createElement("li");
+    //create structure and element first before logic
+    const enteredText = "Roll" + numberOfRolls + ":" + rolledNumber;
+    newRollList.textContent = enteredText;
+    hasRolled = rolledNumber == enteredNumber; // == operator will return a boolean so it loop until it is true
+    rollsList.append(newRollList);
+  }
+
+  const totalRolls = document.getElementById("output-total-rolls");
+  const targetNumber = document.getElementById("output-target-number");
+
+  /* output here respectively */
+  totalRolls.textContent = numberOfRolls;
+  targetNumber.textContent = enteredNumber;
+}
+
+diceBtn.addEventListener("click", deriveNumberOfRolls);
